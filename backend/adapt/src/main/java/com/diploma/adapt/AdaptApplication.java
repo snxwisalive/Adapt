d0493aae7722e155b49d778ebef186ef8f145b8e
deadsnxw <maks.roskoshenko@gmail.com>
2026-04-03 16:56:26 +0300

    init: project structure, spring boot backend, react frontend, docker compose

---
+++ backend/adapt/src/main/java/com/diploma/adapt/AdaptApplication.java
@@ -0,0 +1,13 @@
+package com.diploma.adapt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdaptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdaptApplication.class, args);
+	}
+
+}
---
+++ backend/adapt/src/test/java/com/diploma/adapt/AdaptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diploma.adapt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdaptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
